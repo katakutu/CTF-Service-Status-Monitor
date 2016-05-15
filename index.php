52d1e74fd59b95d9e19d69947cda7117223ffc55
Logan Simpson <logan@logansimpson.xyz>
2016-05-15 00:15:49 -0400

    Shortened code by making grid work off of a loop

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<style>
-
-h1{
-
-text-align: left;
-
-}
-
-h4{
-
-text-align: left;
-
-}
-</style>
 <h1 style="text-align: center;">SHODAN Service Monitor</h1>
 <a href='http://www.github.com/logepoge1' style="text-align: right; position:absolute;top:5px;right:5px;"><img src="GitHub-Mark-32px.png"/></a>
 <link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="style.css" rel=stylesheet>
 <?php
 
 date_default_timezone_set('America/Chicago');
@@ -31,6 +18,7 @@ $ssh = 22;
 $mysql = 3306;
 $apache = 80;
 
+//Pulls the variables from the config.ini file. This is where you set teams
 $shodan = $ini['shodan'];
 $bowser = $ini['bowser'];
 $funky = $ini['funky'];
@@ -39,6 +27,20 @@ $glados = $ini['glados'];
 $kirby = $ini['kirby'];
 $rob = $ini['rob'];
 $conker = $ini['conker'];
+
+//The initialization of this multidimensional array is crucial
+//for the inner array, use the following template:
+//array("Header Team Name", "Matches variables above", "$ActualVariable", "Image location")
+$teamArray = array(
+	array("Shodan", "$shodan", $shodan, "shodan.png"),
+	array("Bowser", "$bowser", $bowser, "bowser.png"),
+	array("Funky Kong", "$funky", $funky, "funky.png"),
+	array("Shadow", "$shadow", $shadow, "shadow.png"),
+	array("GLaDOS", "$glados", $glados, "glados.png"),
+	array("Kirby", "$kirby", $kirby, "kirby.png"),
+	array("R.O.B.", "$rob", $rob, "rob.png"),
+	array("Conker", "$conker", $conker, "conker.png")
+);
 
 function getProperColor($value)
 {
@@ -80,116 +82,25 @@ right:15px;
 }
 </style>
 
-<div class="row">
-  <div class="col-sm-3">
-        <h1>SHODAN</h1>
-	<h4><?php echo $shodan; ?></h4>
-	<img src="images/shodan.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shodan, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shodan, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shodan, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shodan, $mysql)); ?>;">&nbsp;</p>
-  </div>  
-  <div class="col-sm-3">
-        <h1>GLaDOS</h1>
-        <h4><?php echo $glados; ?></h4>
-        <img src="images/glados.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($glados, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($glados, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($glados, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($glados, $mysql)); ?>;">&nbsp;</p>
-  </div>  
-  <div class="col-sm-3">
-        <h1>Bowser</h1>
-        <h4><?php echo $bowser; ?></h4>
-        <img src="images/bowser.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($bowser, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($bowser, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($bowser, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($bowser, $mysql)); ?>;">&nbsp;</p>
-  </div>  
-  <div class="col-sm-3">
-        <h1 style="text-align:left;">Funky Kong</h1>
-        <h4><?php echo $funky; ?></h4>
-        <img src="images/funky.png" class='thumbnail-icon' style="height:120px;"/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($funky, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($funky, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($funky, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($funky, $mysql)); ?>;">&nbsp;</p>
-  </div>
-</div>
+<?php
 
-<div class="row">
-
-  <div class="col-sm-3">
-        <h1>Shadow</h1>
-        <h4><?php echo $shadow; ?></h4>
-        <img src="images/shadow.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shadow, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shadow, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shadow, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($shadow, $mysql)); ?>;">&nbsp;</p>
-  </div>
-    <div class="col-sm-3">
-        <h1>Kirby</h1>
-        <h4><?php echo $kirby; ?></h4>
-        <img src="images/kirby.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($kirby, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($kirby, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($kirby, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($kirby, $mysql)); ?>;">&nbsp;</p>
-  </div>
-    <div class="col-sm-3">
-        <h1>R.O.B.</h1>
-        <h4><?php echo $rob; ?></h4>
-        <img src="images/rob.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($rob, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($rob, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($rob, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($rob, $mysql)); ?>;">&nbsp;</p>
-  </div>
-    <div class="col-sm-3">
-        <h1>Conker</h1>
-        <h4><?php echo $conker; ?></h4>
-        <img src="images/conker.png" class='thumbnail-icon'/>
-        <b>FTP</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($conker, $ftp)); ?>;">&nbsp;</p>
-        <b>SSH</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($conker, $ssh)); ?>;">&nbsp;</p>
-        <b>Apache</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($conker, $apache)); ?>;">&nbsp;</p>
-        <b>MySQL</b>
-        <p style="background-color: <?php echo getProperColor(pingServer($conker, $mysql)); ?>;">&nbsp;</p>
-  </div>
-
-</div>
+echo '<div class="row">';
+for($j=0; $j<count($teamArray); $j++){
+  echo '<div class="col-sm-3">';
+        echo '<h1>' . $teamArray[$j][0] . '</h1>';
+		echo '<h4>' . $teamArray[$j][2] . '</h4>';
+		echo '<img src="images/' . $teamArray[$j][3] . '" class="thumbnail-icon"/>';
+        echo '<b>FTP</b>';
+        echo '<p style="background-color: ' . getProperColor(pingServer($teamArray[$j][1], $ftp)) . '">&nbsp;</p>';
+        echo '<b>SSH</b>';
+        echo '<p style="background-color: ' . getProperColor(pingServer($teamArray[$j][1], $ssh)) . '">&nbsp;</p>';
+        echo '<b>Apache</b>';
+        echo '<p style="background-color: ' . getProperColor(pingServer($teamArray[$j][1], $apache)) . '">&nbsp;</p>';
+        echo '<b>MySQL</b>';
+        echo '<p style="background-color: ' . getProperColor(pingServer($teamArray[$j][1], $mysql)) . '">&nbsp;</p>';
+  echo '</div>'; 
+  }
+echo '</div>';
+?>
 
 <div class="footer" style="bottom:0px;text-align:center;"><b>Created By <a href="http://www.logansimpson.xyz">Logan Simpson</a></b></div>
